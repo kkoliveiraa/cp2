9c549b38f6aef501ec4f295bc3a3ce4ed95d8638
kkoliveiraa <RM550815@fiap.com.br>
2023-09-06 19:08:21 -0300

    tentando arrumar um erro do DOM

--- cp2/src/main.jsx
+++ cp2/src/main.jsx
@@ -1,10 +1,46 @@
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Home from './routes/home.jsx'
+import Error from './routes/error404.jsx'
+import Produtos from './routes/produtos.jsx'
+import EditarProdutos from './routes/EditarProdutos.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/Produtos",
+        element:<Produtos/>
+      },
+      {
+        path: "Produtos/EditarProdutos/:id",
+        element: <EditarProdutos/>,
+       
+      },
+      {
+        path: "/Antiga",
+        element: <Navigate to ="/"/>
+      }
+    ]
+  }  
+  
+]
+
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router ={router} />
   </React.StrictMode>,
 )
+
